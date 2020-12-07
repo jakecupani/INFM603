939dc1bf3000e655ba4d561f11af2ee6a5bd7840
Jake Cupani <jakecup1@gmail.com>
2020-12-06 21:26:09 -0500

    major backend changes

--- nasaswyk/quiz/models.py
+++ nasaswyk/quiz/models.py
@@ -1,12 +1,34 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_title
+
+
+    DIFFICULTIES = [
+        ('Easy','Easy'),
+        ('Medium','Medium'),
+        ('Hard','Hard')
+    ]
+
+    TOPICS = [
+        ("Other","Other"),
+        ("Artemis",'Artemis'),
+    ]
+
+    question_title = models.CharField(default='',max_length=1000)
+    question_description = models.TextField(default="")
+    question_reading = models.TextField(default="")
+    question_topic = models.CharField(default="Other",choices=TOPICS,max_length=100)
+    question_difficulty = models.CharField(default="Easy",choices=DIFFICULTIES,max_length=100)
 
 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=1000)
+    
--- nasaswyk/quiz/views.py
+++ nasaswyk/quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from quiz.models import Question, Choice
 
 
 # Create your views here.
@@ -7,4 +8,8 @@ def quiz(request):
     # get all the quizzes
     # filter by selected difficulty
     context = {}
+    questions = Question.objects.all()
+    context['topics'] = ["Other","Artemis"]
+    context['questions'] = questions
+    print(questions)
     return render(request,"quiz.html",context)
