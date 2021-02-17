15da87affc1a51502df10bc397e0e84458cf0beb
Jake Cupani <jakecup1@gmail.com>
2021-02-17 01:06:25 -0500

    Update settings.py

--- nasaswyk/nasaswyk/settings.py
+++ nasaswyk/nasaswyk/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a+52b41mm-rt5q7dtn#kx-eh773drru&8)cediwb@d$u@8(j)6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
